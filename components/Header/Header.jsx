8ed9a231cf29072cca19f32ef6fa1d68015be184
Mohd Aadil Rana <epochbrokers2gmail.com>
2023-03-29 17:19:20 +0530

    Navbar Created at desktop view

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+import { BsCodeSlash } from "react-icons/bs";
+
 
 const Header = () => {
-    return <div>Header</div>;
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const Close = () => setClick(false);
+    return (
+        <div>
+            <div
+                className={click ? "main-container" : ""}
+                onClick={() => Close()}
+            />
+            <nav className="navbar" onClick={(e) => e.stopPropagation()}>
+                <div className="nav-container">
+                    <Link href="/" className="nav-logo">
+                        AR Dev
+                        <BsCodeSlash />
+                    </Link>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link
+                                href="/"
+                                className="nav-links"
+                                onClick={click ? handleClick : null}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                href="/about"
+                                className="nav-links"
+                                onClick={click ? handleClick : null}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                href="/blog"
+                                className="nav-links"
+                                onClick={click ? handleClick : null}
+                            >
+                                Blog
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                href="/contact"
+                                className="nav-links"
+                                onClick={click ? handleClick : null}
+                            >
+                                Contact Us
+                            </Link>
+                        </li>
+                    </ul>
+                    <div className="nav-icon" onClick={handleClick}>
+                        <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
 };
 
 export default Header;
