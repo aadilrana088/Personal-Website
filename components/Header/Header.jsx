da97625821701da543aa8a0dac135f8d2d2aa390
Mohd Aadil Rana <epochbrokers2gmail.com>
2023-03-30 17:47:38 +0530

    Navbar react scroll added

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import React, { useState } from "react";
 import { BsCodeSlash } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { AiOutlineClose } from "react-icons/ai";
-
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Header = () => {
     const [click, setClick] = useState(false);
@@ -24,43 +23,91 @@ const Header = () => {
                     <ul className={click ? "nav-menu active" : "nav-menu"}>
                         <li className="nav-item">
                             <Link
-                                href="/"
+                                to="introduction"
+                                activeClass="active"
+                                spy={true}
+                                smooth={true}
+                                offset={-80}
+                                duration={200}
                                 className="nav-links"
                                 onClick={click ? handleClick : null}
                             >
-                                Home
+                                Introduction
                             </Link>
                         </li>
                         <li className="nav-item">
                             <Link
-                                href="/about"
+                                to="experience"
+                                activeClass="active"
+                                spy={true}
+                                smooth={true}
+                                offset={-80}
+                                duration={200}
                                 className="nav-links"
                                 onClick={click ? handleClick : null}
                             >
-                                About
+                                Experience
                             </Link>
                         </li>
                         <li className="nav-item">
                             <Link
-                                href="/blog"
+                                to="education"
+                                activeClass="active"
+                                spy={true}
+                                smooth={true}
+                                offset={-80}
+                                duration={200}
                                 className="nav-links"
                                 onClick={click ? handleClick : null}
                             >
-                                Blog
+                                Education
                             </Link>
                         </li>
                         <li className="nav-item">
                             <Link
-                                href="/contact"
+                                to="skills"
+                                activeClass="active"
+                                spy={true}
+                                smooth={true}
+                                offset={-80}
+                                duration={200}
                                 className="nav-links"
                                 onClick={click ? handleClick : null}
                             >
-                                Contact Us
+                                Skills
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="tools"
+                                activeClass="active"
+                                spy={true}
+                                smooth={true}
+                                offset={-80}
+                                duration={200}
+                                className="nav-links"
+                                onClick={click ? handleClick : null}
+                            >
+                                Tools
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="projects"
+                                activeClass="active"
+                                spy={true}
+                                smooth={true}
+                                offset={-80}
+                                duration={200}
+                                className="nav-links"
+                                onClick={click ? handleClick : null}
+                            >
+                                Projects
                             </Link>
                         </li>
                     </ul>
                     <div className="nav-icon" onClick={handleClick}>
-                        <>{click ? <AiOutlineClose /> : <RxHamburgerMenu /> }</>
+                        <>{click ? <AiOutlineClose /> : <RxHamburgerMenu />}</>
                     </div>
                 </div>
             </nav>
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,24 @@ import Layout from "../components/layout";
 export default function Home() {
     return (
         <Layout>
-            <p>This is Home page</p>
+            <section className="introduction" id="introduction" title="introduction">
+                introduction
+            </section>
+            <section className="experience" id="experience" title="experience">
+                experience
+            </section>
+            <section className="education" id="education" title="education">
+                education
+            </section>
+            <section className="skills" id="skills" title="skills">
+                skills
+            </section>
+            <section className="projects" id="projects" title="projects">
+                projects
+            </section>
+            <section className="tools" id="tools" title="tools">
+                tools
+            </section>
         </Layout>
     );
 }
